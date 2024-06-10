7260bd55d223670eeeee58191f4778fb3bee7a99
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-10 09:16:38 +0000

    add props

--- Organo/src/App.jsx
+++ Organo/src/App.jsx
@@ -1,12 +1,16 @@
-import Banner from "./componentes/Banner/Banner";
+import React from 'react';
+import Banner from "./componentes/Banner";
+import CampoTexto from "./componentes/CampoTexto";
 
 function App() {
-
   return (
     <>
-        <Banner/>
+      <Banner />
+      <CampoTexto label="Nome" placeholder="Digite nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o enderço da imagem"/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

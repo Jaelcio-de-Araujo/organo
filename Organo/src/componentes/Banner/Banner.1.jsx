4830696fcbc724619875eb8f73c9bf060091f111
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-10 01:59:31 +0000

    create components

---
+++ Organo/src/componentes/Banner/Banner.1.jsx
@@ -0,0 +1,5 @@
+export function Banner() {
+    return {}
+        < img; src = "./imagens/Banner.png"; alt = "Banner Principal da Pagina" /  >
+    ;
+}
---
+++ Organo/src/componentes/Banner/Banner.jsx
@@ -0,0 +1,7 @@
+import BannerImage from "../../../public/imagens/banner.png";
+
+function Banner() {
+    return <img src={BannerImage} alt="Banner do site" />;
+}
+
+export default Banner;

4f8f52e77c00c817c8e68fd4fa1179d3e766ef99
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-10 02:39:28 +0000

    fix App.jsx

--- Organo/src/App.jsx
+++ Organo/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import "./componentes/Banner/Banner.jsx";
+import "./componentes/Banner/Banner";
 
 function App() {
   const [count, setCount] = useState(0)

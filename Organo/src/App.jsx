98310d0ae25374d70af7f215cd9517bd1fcbaca1
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-10 02:35:15 +0000

    refatoring component

--- Organo/src/App.jsx
+++ Organo/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Banner from './componentes/Banner/Banner'
+import "./componentes/Banner/Banner.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <Banner />
+        <Banner/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

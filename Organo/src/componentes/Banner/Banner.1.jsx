0e0ca13a80105d1747265704e9fac5c42f3eda44
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-10 02:35:56 +0000

    fix component

--- Organo/src/componentes/Banner/Banner.1.jsx
+++
@@ -1,5 +0,0 @@
-export function Banner() {
-    return {}
-        < img; src = "./imagens/Banner.png"; alt = "Banner Principal da Pagina" /  >
-    ;
-}
--- Organo/src/componentes/Banner/Banner.jsx
+++ Organo/src/componentes/Banner/Banner.jsx
@@ -1,7 +1,11 @@
 import BannerImage from "../../../public/imagens/banner.png";
+import './Banner.css';
 
 function Banner() {
-    return <img src={BannerImage} alt="Banner do site" />;
+    return (
+        <header className="banner">
+            <img src={BannerImage} alt="Banner do site" />
+        </header>
+    );
 }
-
 export default Banner;

3603f36c613ac0900e3a25a8562e24460b3565dc
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-12 00:54:47 +0000

    create card

--- Organo/src/componentes/Formulario/index.jsx
+++ Organo/src/componentes/Formulario/index.jsx
@@ -1,13 +1,25 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
     return (
-        <section>
+        <section className="formulario">
             <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
